cd958319900d154925ed7109fa92cbe52c0acd3b
I.Smirnov <I.Smirnov@919LC63>
2021-11-07 18:16:12 +0300

    Добавьте файлы проекта.

---
+++ Задание 5.5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = n;
+            int[,] t = new int[n, m];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    t[i, j] = random.Next(0, 2);
+                    Console.Write("{0, 4}", t[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            Console.WriteLine("Введите число 0");
+            array[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число 1");
+            array[1] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число 2");
+            array[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число 3");
+            array[3] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число 4");
+            array[4] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число 5");
+            array[5] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число 6");
+            array[6] = Convert.ToInt32(Console.ReadLine());
+            int S = (array[0] + array[1] + array[2] + array[3] + array[4] + array[5] + array[6])/7;
+            Console.Write("Среднее арифметическое {0}", S);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
